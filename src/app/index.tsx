78eae9dff055ce241726989c7d772764cf4811fe
luis.fib <luis.fib@hotmail.com>
2024-06-20 19:03:08 -0300

    :tada: initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native"
+
+export default function Home(){
+    return(
+        <View className=" flex-1 bg-green-500 items-center justify-center" >
+            <Text className="bg-gray-100">Hello Ass</Text>
+        </View>
+    )
+}
